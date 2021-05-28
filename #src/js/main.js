e8e5743ddc289ffdb2e71e3ab09f84544f3b4418
schklovchik_denis <schklovchik_den@mail.ru>
2021-05-28 22:54:46 +0500

    Delete #src directory

--- #src/js/main.js
+++
@@ -1,4 +0,0 @@
-@@include('jq-start.js')
-@@include('forms.js')
-@@include('script.js')
-@@include('jq-end.js')
--- #src/js/vendors.js
+++
@@ -1,7 +0,0 @@
-@@include('jquery.inputmask.bundle.min.js') 
-@@include('jquery.nicescroll.min.js')
-@@include('jquery.ui.touch-punch.min.js')
-@@include('jquery.popover.min.js')
-@@include('baguetteBox.js')
-// @@include('scroll.js')
-@@include('slick.min.js')
